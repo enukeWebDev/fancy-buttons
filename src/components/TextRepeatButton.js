3b603dfe6abe1e63dd2c5c692f39ea2b60d3fe1d
Roderick <enuke4@gmail.com>
2022-01-17 14:16:42 -0600

    add function to handle the click for each components

--- src/components/TextRepeatButton.js
+++ src/components/TextRepeatButton.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 
 function TextRepeatButton(props) {
   const [repetitions, setRepetitions] = useState(1);
+
   const textArray = [];
   for (let i = 0; i < repetitions; i++) {
     textArray.push(<span key={i}>I like this text</span>);
   }
 
+  const handleClick = () => setRepetitions(repetitions + 1);
+
   return (
-    <button classname="TextRepeatButton">
+    <button onClick={handleClick} className="TextRepeatButton">
       {textArray}
     </button>
   );
