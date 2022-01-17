f9f27f1e83213dda4dc3f2408d68784c610388c1
Roderick <enuke4@gmail.com>
2022-01-17 14:36:13 -0600

    lift LightSwitchButton component state to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeatButton from './components/TextRepeatButton';
 
 function App() {
+  const [light, setLight] = useState('off');
+
   return (
     <div className="App">
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight} />
         <TextRepeatButton />
       </section>
     </div >
